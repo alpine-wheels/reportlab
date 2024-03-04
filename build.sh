7b4703b27ffce82291b07e61fa4b1e0eb0ac862b
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-03-04 16:26:25 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache freetype-dev gcc mus-dev
+apk add --no-cache freetype-dev gcc musl-dev
 
 # build
 pip wheel --no-deps --requirement requirements.txt
